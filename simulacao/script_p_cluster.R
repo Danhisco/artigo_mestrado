71c345c4d8f745aa430b15222947d8c1bfcc9875
danhisco <danilo.mori@usp.br>
2019-09-21 21:30:50 -0300

    sinteses dos dados corrigida

--- simulacao/script_p_cluster.R
+++ simulacao/script_p_cluster.R
@@ -12,14 +12,15 @@ setwd("/home/danilo/Documentos/Doutorado/artigo_mestrado/simulacao/")
 source("dinamica_coalescente_beta.R") 
 # dados
 df_referencia <- read.csv(file="df_referencia.csv",row.names = 1,as.is = TRUE)
-df_referencia %<>% filter(S %in% c(195,230,226,26,45,52))
+# df_referencia %<>% filter(S %in% c(195,230,226,26,45,52)) # sítios com S extremos em cada terço do gradiente de p
+# df_referencia %<>% filter(k %in% c(0.99,0.5,0.05))
 # ddply("quantil_p",summarise,S_max=max(S),S_min=min(S))
 
 # df_referencia %>% str
 
-##### número de núcleos do computador #####
-n_cores <- 3
-
+##### padronização do sistema #####
+n_cores <- 2 # número de cores do computador 
+n_rep <- 100 # nũmero de  
 ######################################################
 #################### MNEE ############################
 ######################################################
@@ -35,7 +36,7 @@ df_referencia %<>% func1()
 ### for da simulação ##
 # valores de k
 k_factor <- unique(df_referencia$k)
-for(a in 19:length(k_factor)){
+for(a in 1:length(k_factor)){
   # a <- 1
   # i <- 1
   # por k
@@ -72,13 +73,13 @@ f_simulacao <- function(i,df_=df_simulacao){
   X <- df_[i,][["data"]] %>% as.data.frame()
   mat_sim <- dinamica_coalescente(U = X[,"U_med"], 
                                   S = 0, 
-                                  N_simul = 100, 
+                                  N_simul = n_rep, 
                                   seed = as.numeric(Sys.time()), 
                                   disp_range = X[,"d"], 
                                   disp_kernel = X[,"kernel_code"], 
                                   landscape = X[,"txt.file"])
 }
-registerDoMC(n_cores)
+registerDoMC(2)
 simulacao <- as.list(1:dim(df_simulacao)[1])
 df_simulacao$SADs.EE  <- llply(simulacao,f_simulacao,.parallel = TRUE) 
 #funcao para escrita das SADs em .csv
@@ -113,8 +114,8 @@ df_simulacao %<>% mutate(L_plot = 100/sqrt(J/DA),
                          theta=(U_med*(J_M-1))/(1-U_med))
 df_simulacao %<>% mutate(k_prop=k) %>%  group_by(SiteCode,k_prop) %>% nest
 ## Predição da SAD
-registerDoMC(n_cores)
-df_simulacao$SADs <- llply(df_simulacao[["data"]],function(X) replicate(100,generate.ESF(theta = X$theta, I = X$I, J = X$J)),.parallel = TRUE)
+registerDoMC(2)
+df_simulacao$SADs <- llply(df_simulacao[["data"]],function(X) replicate(n_rep,generate.ESF(theta = X$theta, I = X$I, J = X$J)),.parallel = TRUE)
 f_d_ply <- function(X){
   # df_name <- df_simulacao[1,]$data %>% as.data.frame
   # l_SADs <-  df_simulacao[1,]$SADs[[1]]
@@ -140,10 +141,11 @@ df_resultados$txt.file %<>% as.character()
 df_resultados %<>% mutate(file.tag=gsub(".txt","",txt.file)) 
 
 ## df_SAD.obs
-df_SAD.obs <- read.csv(file = "/home/danilo/Documentos/Doutorado/artigo_mestrado/simulacao/SADs_observadas/df_SAD_obs.csv") 
+df_SAD.obs <- read.csv(file = "/home/danilo/Documentos/Doutorado/artigo_mestrado/simulacao/SAD_observada/abundances.csv") 
 # df_SAD.obs %>% dim
-df_SAD.obs %<>% filter(N!=0 & sp != "Mortas") %>% group_by(SiteCode) %>% nest
+df_SAD.obs %<>% group_by(SiteCode) %>% nest
 names(df_SAD.obs)[2] <- "SAD.obs"
+
 
 ## df_SAD.predita ##
 df_SAD.predita <- data.frame(SAD_MN.name=as.character(Sys.glob("./SADs_preditas/*.csv")))
@@ -186,11 +188,12 @@ f_resultados <- function(X){
 registerDoMC(n_cores)
 df_SAD.predita %<>% ddply(.,"SiteCode",f_resultados,.parallel = TRUE)
 write.csv(df_SAD.predita,file="./resultados/df_replicas.csv",row.names = F)
-df_SAD.predita %<>% ddply(.,c("SiteCode","MN","k"),summarise,
-                          GOF=length(KS.p>=0.05),
+df_SAD.predita <- read.csv("./resultados/df_replicas.csv")
+df_SAD.predita %<>% ddply(.,c("SiteCode","MN","k","S_SAD.obs"),summarise,
+                          GOF=sum(KS.p>=0.05),
                           p.value_mean=mean(KS.p),p.value_var=var(KS.p),
-                          S_mean=mean(S_SAD.predita),S_var=var(S_SAD.predita),S.obs_mean=mean(S_SAD.obs))
-df_resultados$k %<>% as.character()
+                          S_mean=mean(S_SAD.predita),S_var=var(S_SAD.predita),
+                          S.obs_SAD=mean(S_SAD.obs))
 df_SAD.predita %<>% left_join(x=.,y=df_resultados[,c(1:6,11,13:14)],by=c("SiteCode","k"))
 write.csv(df_SAD.predita,file="./resultados/df_resultados.csv",row.names = F)
 
@@ -208,6 +211,20 @@ f_z <- function(x){
 }
 df_resultados %<>% mutate(p.z = f_z(p),S.z = f_z(S))
 names(df_resultados)[1] <- "Site"
+
+########## Auditoria dos Dados ############
+df_resultados %>% mutate(S.obs = S.obs_mean) %>%  
+  ggplot(aes(x=S,y=S.obs)) + 
+  geom_point() + 
+  geom_abline(intercept = 0,slope=1,color="red") +
+  labs(x="S planilha references on line",
+       y="S vetor utilizado em KS")
+df_resultados%>% head
+# df_auditoria <- df_resultados %>% mutate(sitecode_difS=S.obs_mean-S) %>% filter(sitecode_difS>0) %>% select(Site,S,S.obs_mean) %>% unique
+# write.csv(df_auditoria,file="./resultados/df_auditoria.csv",row.names = FALSE)
+
+
+
 
 ###################### GOF ######################
 
