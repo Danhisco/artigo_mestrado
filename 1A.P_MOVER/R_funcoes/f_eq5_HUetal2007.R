70fc1a520e6bcacdb3096d23f788e0054a9a6f20
Danilo Mori <dnlpmo@gmail.com>
2022-06-20 16:29:57 -0300

    source da função para aplicar a equação 5 de Hu et al. 2007

---
+++ 1A.P_MOVER/R_funcoes/f_eq5_HUetal2007.R
@@ -0,0 +1,15 @@
+f_eq5_HUetal2007 <- function(x, speciation, migration_rate, JL, JM){
+  small_theta <- 2*JL*speciation
+  capital_theta <- 2*JM*speciation
+  migrants <- JL*migration_rate
+  f1 <- function(y) (1-y)^(capital_theta-1)/y
+  .C <- 1/integrate(f1,1/JM, 1)$value
+  c1 <- function(Q){
+    lc1 <- lgamma(2*migrants+small_theta+1) -
+      (lgamma(2*migrants*(1-Q)+small_theta) + lgamma(2*migrants*Q+1))
+    exp(lc1)
+  }
+  f2 <- function(Q)
+    c1(Q)*(1-x)^(2*migrants*(1-Q)+small_theta-1)*x^(2*migrants*Q-1)*(1-Q)^(capital_theta-1)/Q
+  .C*integrate(f2, 1/JM, 1)$value
+}
